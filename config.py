f1644454fede35f0e370746451913f2d8a304849
Pzzzzz <hello-cd.plus@hotmail.com>
2020-04-13 15:01:40 +0800

    。

---
+++ config.py
@@ -0,0 +1,8 @@
+from nonebot.default_config import *
+
+SUPERUSERS = {545870222}
+COMMAND_START = {'', '/', '!', '／', '！'}
+HOST = '0.0.0.0'
+PORT = 6700
+DEBUG = False
+API_ROOT = 'http://127.0.0.1:5700'
