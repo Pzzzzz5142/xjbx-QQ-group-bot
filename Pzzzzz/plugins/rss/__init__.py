7131cfa84ab246c4accbcfa63df77c524ac30fe8
Pzzzzz <hello-cd.plus@hotmail.com>
2020-05-22 18:49:59 +0800

    重构rss命令parse方法

--- Pzzzzz/plugins/rss/__init__.py
+++ Pzzzzz/plugins/rss/__init__.py
@@ -53,15 +53,7 @@ async def __():
 
 @on_command("rss", only_to_me=False)
 async def rss(session: CommandSession):
-
-    if session.state["ls"] == []:
-        session.pause(
-            "请输入你想「{0}」的资讯！\n输入 mrfz 代表 「明日方舟」\n输入 bcr 代表 「公主链接 B服」\n输入 gcores 代表 「机核网」\n如果有多个想{0}的资讯源可以在一行中输入多个并以空格分开！".format(
-                "查看" if session.state["subs"] == 0 else "订阅"
-            )
-        )
-
-    if session.state["subs"] == 1:
+    if "subs" in session.state:
         async with db.pool.acquire() as conn:
             for _, item in session.state["ls"]:
                 try:
@@ -83,20 +75,64 @@ async def rss(session: CommandSession):
                     logger.error("some rss issue", exc_info=True)
 
     elif "route" in session.state:
-        bot = nonebot.get_bot()
         for rt in session.state["ls"]:
             resp = await sendrss(
-                session.event.user_id, bot, "自定义路由", None, getrss, (1, 1), route=rt,
+                session.event.user_id,
+                session.bot,
+                "自定义路由",
+                None,
+                getrss,
+                (1, 1),
+                route=rt,
             )
             if resp and session.event.detail_type != "private":
                 await session.send(
                     unescape(cq.at(session.event.user_id) + f"「{rt}」的资讯已私信，请查收。")
                 )
 
+    elif "del" in session.state:
+        async with db.pool.acquire() as conn:
+            fail = []
+            success = []
+            for _, dl in session.state["ls"]:
+                resp = await conn.execute(
+                    "delete from subs where qid = {} and rss = '{}'".format(
+                        session.event.user_id, dl
+                    )
+                )
+                if resp[len("delete ") :] == "0":
+                    fail.append(doc[dl])
+                else:
+                    success.append(doc[dl])
+            if len(fail) > 0:
+                await session.send(
+                    cq.at(session.event.user_id)
+                    + f"这{'个' if len(fail)==1 else '些'}源「{' '.join(fail)}」不在你的订阅列表里面哦～"
+                )
+            if len(success) > 0:
+                await session.send(
+                    cq.at(session.event.user_id)
+                    + f" 取消订阅「{' '.join(success)}」成功！可喜可贺，可喜可贺！"
+                )
+    elif session.state["list"]:
+        async with db.pool.acquire() as conn:
+            values = await conn.fetch(
+                "select * from subs where qid = {}".format(session.event.user_id)
+            )
+            if len(values) == 0:
+                session.finish("貌似你没有订阅任何 rss 源")
+            await session.send(
+                cq.at(session.event.user_id)
+                + "以下是你已订阅的源：\n{}".format(
+                    "\n".join([doc[i["rss"]] + " - " + i["rss"] for i in values])
+                )
+            )
+
     else:
-        bot = nonebot.get_bot()
         for item, nm in session.state["ls"]:
-            resp = await sendrss(session.event.user_id, bot, nm, None, item, (1, 1))
+            resp = await sendrss(
+                session.event.user_id, session.bot, nm, None, item, (1, 1)
+            )
             if resp and session.event.detail_type != "private":
                 await session.send(
                     unescape(cq.at(session.event.user_id) + f"「{doc[nm]}」的资讯已私信，请查收。")
@@ -105,82 +141,105 @@ async def rss(session: CommandSession):
 
 @rss.args_parser
 async def _(session: CommandSession):
-    arg = session.current_arg_text.strip()
-    args = arg.split(" ")
-    args = [i for i in args if i != ""]
-
     if session.is_first_run:
-        parser=ArgumentParser(session=session)
-        subparser=parser.add_mutually_exclusive_group()
-        subparser.add_argument('-s','--subs',help='订阅指定的 rss 源')
-        subparser.add_argument('-r','--route',help='获取指定 rss 源的资讯')
-        subparser.add_argument('-d','--delete',help='删除 rss 订阅')
-        parser.add_argument('ls',help='要处理的 rss 列表',nargs='+')
-        argv=parser.parse_args(args)
+        parser = ArgumentParser(session=session)
+        subparser = parser.add_mutually_exclusive_group()
+        subparser.add_argument("-s", "--subs", nargs="+", help="订阅指定的 rss 源")
+        subparser.add_argument("-r", "--route", nargs="+", help="获取自定路由的 rss 源的资讯")
+        subparser.add_argument("-d", "--delete", nargs="+", help="删除 rss 订阅")
+        subparser.add_argument(
+            "-l", "--list", action="store_true", default=False, help="列出已订阅的源"
+        )
+        parser.add_argument("rss", nargs="*", help="获取已存在的 rss 源资讯")
+        argv = parser.parse_args(session.current_arg_text.strip().split(" "))
         session.state["ls"] = []
-        session.state["subs"] = 1 if (len(argv.ls) > 0 and argv.subs) else 0
+        session.state["list"] = argv.list
+        if argv.list:
+            return
+        if argv.subs != None:
+            session.state["subs"] = argv.subs
+            ls = argv.subs
+        if argv.delete != None:
+            session.state["del"] = argv.delete
+            ls = argv.delete
+        if argv.rss != []:
+            session.state["rss"] = argv.rss
+            ls = argv.rss
         if argv.route != None:
-            session.state["route"] = "ok"
-            session.state["ls"] = [x for x in argv.ls]
+            session.state["route"] = argv.route
+            session.state["ls"] = argv.route
             if len(session.state["ls"]) == 0:
                 session.finish("查询路由地址不能为空哦！")
             return
-        if "-s" in args:
-            args.remove("-s")
 
-    if "mrfz" in args:
+    if "mrfz" in ls:
         session.state["ls"].append((getmrfz, "mrfz"))
-        args.remove("mrfz")
+        ls.remove("mrfz")
 
-    if "bcr" in args:
+    if "bcr" in ls:
         session.state["ls"].append((getbcr, "bcr"))
-        args.remove("bcr")
+        ls.remove("bcr")
 
-    if "gcores" in args:
+    if "gcores" in ls:
         session.state["ls"].append((getgcores, "gcores"))
-        args.remove("gcores")
+        ls.remove("gcores")
 
-    if "loli" in args:
+    if "loli" in ls:
         session.state["ls"].append((getloli, "loli"))
-        args.remove("loli")
+        ls.remove("loli")
 
-    if "pprice" in args:
+    if "pprice" in ls:
         session.state["ls"].append((getpprice, "pprice"))
-        args.remove("pprice")
+        ls.remove("pprice")
 
-    if "bh3" in args:
+    if "bh3" in ls:
         session.state["ls"].append((getbh3, "bh3"))
-        args.remove("bh3")
+        ls.remove("bh3")
 
-    if "hpoi" in args:
+    if "hpoi" in ls:
         session.state["ls"].append((gethpoi, "hpoi"))
-        args.remove("hpoi")
+        ls.remove("hpoi")
 
-    if len(args) > 0:
+    if len(ls) > 0:
         await session.send(
             unescape(
-                "没有添加「{}」的订阅源！请联系".format(" ".join(args)) + cq.at(545870222) + "添加订阅！"
+                "没有添加「{}」的订阅源！请联系".format(" ".join(ls)) + cq.at(545870222) + "添加订阅！"
             )
         )
-    if len(session.state["ls"]) == 0 and (not session.is_first_run or arg != ""):
+    if len(session.state["ls"]) == 0:
         session.finish(
-            "本次资讯{}为空哦！".format("查看" if session.state["subs"] == 0 else "订阅")
+            "本次资讯{}为空哦！".format("查看" if session.state["rss"] != [] else "订阅")
         )
 
 
 @on_command("订阅", only_to_me=False, shell_like=True)
 async def subs(session: CommandSession):
-    args = session.current_arg_text.strip(" ")
+    ls = session.current_arg_text.strip(" ")
 
     flg = await call_command(
         session.bot,
         session.event,
         "rss",
-        current_arg="-s " + args,
+        current_arg="-s " + ls,
         disable_interaction=True,
     )
     if flg == False:
         session.finish("订阅失败")
+
+
+@on_command("取消订阅", only_to_me=False, shell_like=True)
+async def unsubs(session: CommandSession):
+    ls = session.current_arg_text.strip(" ")
+
+    flg = await call_command(
+        session.bot,
+        session.event,
+        "rss",
+        current_arg="-d " + ls,
+        disable_interaction=True,
+    )
+    if flg == False:
+        session.finish("取消订阅失败")
 
 
 @on_command("up", only_to_me=False, shell_like=True, permission=perm.SUPERUSER)
